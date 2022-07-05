20f0597b82e6a0cee8b35028fb84284d6efc4484
ALI11IB <ali.la.ibrahem@gmail.com>
2022-07-05 22:58:17 +0300

    add pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+import { Home, Game } from "./pages";
 
 function App() {
-  return <div className="flex justify-center">hello</div>;
+  return (
+    <div className="max-w-7xl mx-auto px-4">
+      <div className="fit">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/:gameId" element={<Game />} />
+        </Routes>
+      </div>
+    </div>
+  );
 }
 
 export default App;
