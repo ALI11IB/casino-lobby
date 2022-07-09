b914656c901feadcf8b2c2aa07ee0543ada439da
ALI11IB <ali.la.ibrahem@gmail.com>
2022-07-09 22:32:12 +0300

    add animation

--- src/components/ui/card/index.tsx
+++ src/components/ui/card/index.tsx
@@ -25,11 +25,19 @@ export default function Card({ game, index }: { game: Game; index: number }) {
           <img
             src="/images/icon2.svg"
             alt="icon1"
-            className="absolute icon1 "
+            className="  absolute icon1 "
           />
-          <img src="/images/icon3.svg" alt="icon2" className="absolute icon2" />
+          <img
+            src="/images/icon3.svg"
+            alt="icon2"
+            className="animate-pulse absolute icon2"
+          />
 
-          <img src="/images/icon2.svg" alt="icon1" className="absolute icon3" />
+          <img
+            src="/images/icon2.svg"
+            alt="icon1"
+            className="animate-pulse absolute icon3"
+          />
           <img
             src="/images/icon3.svg"
             alt="icon2"
@@ -37,7 +45,7 @@ export default function Card({ game, index }: { game: Game; index: number }) {
           />
         </div>
         <div className="w-1/2 h-full flex flex-col justify-between">
-          <div className="mt-4 mr-2 ml-3 tracking-wide text-lg text-white italic font-extrabold">
+          <div className="animate-pulse mt-4 mr-2 ml-3 tracking-wide text-lg text-white italic font-extrabold">
             {game.name}
           </div>
 
@@ -48,8 +56,10 @@ export default function Card({ game, index }: { game: Game; index: number }) {
             by {game.provider}
           </p>
           <div className="ml-2 pt-2 pb-2 bg-zinc-900 trapozid ">
-            <p className=" text-sm sm:text-sm text-gray-300">{game.tags[0]}</p>
-            <p className=" text-sm sm:text-sm text-gray-300">
+            <p className="animate-pulse text-sm sm:text-sm text-gray-300">
+              {game.tags[0]}
+            </p>
+            <p className="animate-pulse text-sm sm:text-sm text-gray-300">
               {game.tags[1]} +{game.tags.length - 2}more
             </p>
           </div>
